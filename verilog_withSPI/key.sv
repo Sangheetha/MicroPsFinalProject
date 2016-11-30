482aa172aeefcd62d489a976809a317b0fb7c478
Nancy Wei <nwei@g.hmc.edu>
2016-11-30 00:15:14 -0800

    Key match working over SPI

--- verilog_withSPI/key.sv
+++ verilog_withSPI/key.sv
@@ -7,53 +7,56 @@ module key_match_fsm(input  logic clk,
 				 input logic timer_done,
 				 output logic key_matches,
 				 output logic seq_reset,
-				 output logic lose_life);
+				 output logic lose_life,
+				 output logic start);
 		   
 // Nancy Wei 11/14/16
 // nwei@g.hmc.edu
 // Determine if player input matches next key in the sequence
-
-
 	
-	typedef enum logic [3:0] {S0,S1,S2,S3,S4,S5} statetype;
+	typedef enum logic [3:0] {S0,S1,S2,S3,S4,S5,S6,S7} statetype;
 	statetype state, next;
 
 	// state register
 	always_ff@(posedge clk, posedge reset)
-		if(reset) state <= S0;
+		if(reset) state <= S0; 
 		else		 state <= next;
 	
 	// next state logic
 	always_comb
 		case(state)
 			// scan state
-			S0: if(timer_done)				next = S5;
+			S0: if(timer_done)						next = S5;
 				else if(seq_done)				      next = S3;
-				else if(key_pressed == next_key)  next = S1;
-				else if (key_pressed != 0)  	  next = S2;
-				else 					     	  next = S0;	
+				else if(key_pressed == next_key) next = S1;
+				else if (key_pressed != 0)  	  	next = S2;
+				else 					     	  			next = S0;	
 				
 			// matched state
-			S1: if(timer_done)				next = S5;	 		 	  
-				 else 							next = S4;
+			S1: if(timer_done)			  next = S5;	 		 	  
+				 else 						  next = S4;
 			
 			// reset state 	 
-			S2: if(timer_done)				next = S5;
-				else								next = S4;
+			S2: if(timer_done)			  next = S5;
+				else							  next = S4;
 			
 			// sequence success state
 			S3: if(timer_done)			  next = S0;
-				  else 						 next = S3;
+				  else 						  next = S3;
 			
 			// wait state
-			S4: if(timer_done) 			 next = S5;	
-				else if(key_pressed == 0) 		 	  next = S0;
+			S4: if(timer_done) 			  next = S5;	
+				else if(key_pressed == 0) next = S0;
 				else 						 	  next = S4;
 				
 			// sequence fail state
-			S5: 									next = S0;
+			S5: 								  next = S0;
 			
-			default:   							  next = S0;
+			// initial state, start game
+			S6: if(key_pressed == 4'h5)  next = S7;
+				 else							  next = S6;
+			S7: 								  next = S0;
+			default:   						  next = S0;
 
 		endcase
 	
@@ -61,6 +64,7 @@ module key_match_fsm(input  logic clk,
 	assign key_matches = (state == S1);
 	assign seq_reset = (state == S2) | timer_done;
 	assign lose_life = (state == S5); 
+	assign start = (state == S7);
 	
 endmodule 
 
@@ -76,15 +80,25 @@ module next_key_gen(input  logic clk,
 // nwei@g.hmc.edu
 // Outputs the next key (key to match) in the sequence					
 
-	logic [3:0] count;
+	logic [3:0] count, index;
 				
 	// Enabled counter, index of the sequence  
 	always_ff@(posedge clk, posedge reset)
 		if(reset) 	count <= 3'b0;
 		else if(seq_reset) count <= 3'b0;
 		else if(count_en) 		count <= count + 3'h4;
+	
+	// Reverse order so key_seq can be written MSB = first key
+	always_comb
+		case(count)
+			4'b0000: index = 4'b1100;
+			4'b0100: index = 4'b1000;
+			4'b1000: index = 4'b0100;
+			4'b1100: index = 4'b0000;
+			default: index = 4'b0000;
+		endcase
 		
-	assign next_key = {key_seq[count+3], key_seq[count+2],key_seq[count+1],key_seq[count]};
+	assign next_key = {key_seq[index+3], key_seq[index+2],key_seq[index+1],key_seq[index]};
 	assign seq_done = (next_key == 4'hF);
 		
 endmodule 	
@@ -92,6 +106,7 @@ endmodule
 module key_seq_gen(input logic clk,
 							input logic reset,
 							input logic timer_done,
+							input logic start,
 							output logic [15:0] key_seq,
 							output logic [1:0] level);
 
@@ -102,13 +117,15 @@ module key_seq_gen(input logic clk,
 	// Enabled counter, current level
     always_ff@(posedge clk, posedge reset)
 		if(reset) 	level <= 2'b0;
-		else if(timer_done) level <= level + 2'b1;
+		else if(timer_done | start) level <= level + 2'b1;
 	
 	always_comb
 		case(level)
-			2'b0: key_seq = 16'hFF23; //LEFT, DOWN
-			2'b1: key_seq = 16'hF224; //RIGHT, DOWN, DOWN
-			2'b10: key_seq = 16'hF212; // DOWN, UP, DOWN 
+			//2'b0: key_seq = 16'h0000; // Default for initial state
+			2'b0: key_seq = 16'h11FF;
+			2'b1: key_seq = 16'h32FF; //LEFT, DOWN, LEFT
+			2'b10: key_seq = 16'h422F; //RIGHT, DOWN, DOWN
+			2'b11: key_seq = 16'h212F; // DOWN, UP, DOWN 
 			default: key_seq = 16'hFFFF;
 		endcase
 
